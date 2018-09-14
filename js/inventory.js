848301ccbca99d9c8bbfca1c3b5ba92fdecb79bb
Mithul Nallaka <Nallaka@users.noreply.github.com>
2018-09-14 15:28:01 -0500

    add item

--- js/inventory.js
+++ js/inventory.js
@@ -1,50 +1,50 @@
 var inventory = firebase.database();
 
 function getInvRef(upc, children) {
-  return inventory.ref('inventory/' + upc.toString() + '/' + children)
+    return inventory.ref('inventory/' + upc.toString() + '/' + children)
 }
 
 function addItem(name, upc, description, binNumber, consumable, totalStock, inStock, onHand) {
-  getInvRef(upc, '').set(createItem(name, upc, description, binNumber, consumable, totalStock, inStock, onHand))
+    getInvRef(upc, '').set(createItem(name, upc, description, binNumber, consumable, totalStock, inStock, onHand))
 }
 
 function removeItem(upc) {
-  getInvRef(upc, '').remove()
+    getInvRef(upc, '').remove()
 }
 
 function getItemByID(upc) {
-  var item = null;
-  getInvRef(upc, '').on('value',
-      function (dataSnapshot) {
-        console.log(dataSnapshot.val());
-        item = dataSnapshot.val()
-      });
+    var item = null;
+    getInvRef(upc, '').on('value',
+        function (dataSnapshot) {
+            console.log(dataSnapshot.val());
+            item = dataSnapshot.val()
+        });
 
-  return item;
+    return item;
 }
 
 function checkOutItem(upc, countToCheckOut) {
-  var item = getItemByID(upc);
+    var item = getItemByID(upc);
 
-  if(countToCheckOut > item.inStock) {
-    console.log('ERROR: Checkout FAILED - Items inStock not sufficient');
-    return false;
-  } else {
-    getInvRef(upc, 'inStock/').set(item.inStock - countToCheckOut);
-    getInvRef(upc, 'onHand/').set(item.onHand + countToCheckOut);
-    return true;
-  }
+    if (countToCheckOut > item.inStock) {
+        console.log('ERROR: Checkout FAILED - Items inStock not sufficient');
+        return false;
+    } else {
+        getInvRef(upc, 'inStock/').set(item.inStock - countToCheckOut);
+        getInvRef(upc, 'onHand/').set(item.onHand + countToCheckOut);
+        return true;
+    }
 }
 
 function checkInItem(upc, countToCheckIn) {
-  var item = getItemByID(upc);
+    var item = getItemByID(upc);
 
-  if(countToCheckIn > item.onHand) {
-    console.log('ERROR: Check-in FAILED - Items onHand not sufficient');
-    return false;
-  } else {
-    getInvRef(upc, 'inStock/').set(item.inStock + countToCheckIn);
-    getInvRef(upc, 'onHand/').set(item.onHand - countToCheckIn);
-    return true;
-  }
+    if (countToCheckIn > item.onHand) {
+        console.log('ERROR: Check-in FAILED - Items onHand not sufficient');
+        return false;
+    } else {
+        getInvRef(upc, 'inStock/').set(item.inStock + countToCheckIn);
+        getInvRef(upc, 'onHand/').set(item.onHand - countToCheckIn);
+        return true;
+    }
 }
--- js/item.js
+++ js/item.js
@@ -1,44 +1,44 @@
 function createItem(name, upc, description, binNumber, consumable, totalStock, inStock, onHand) {
-  return {
-    name: name,
-    upc: upc,
-    description: description,
-    binNumber: binNumber,
-    consumable: consumable,
-    totalStock: totalStock,
-    inStock: inStock,
-    onHand: onHand
-  }
+    return {
+        name: name,
+        upc: upc,
+        description: description,
+        binNumber: binNumber,
+        consumable: consumable,
+        totalStock: totalStock,
+        inStock: inStock,
+        onHand: onHand
+    }
 }
 
 function getItemName(upc) {
-  console.log(getItemByID(upc).name)
+    console.log(getItemByID(upc).name)
 }
 
 function getItemUpc(upc) {
-  console.log(getItemByID(upc).upc)
+    console.log(getItemByID(upc).upc)
 }
 
 function getItemDescription(upc) {
-  console.log(getItemByID(upc).description)
+    console.log(getItemByID(upc).description)
 }
 
 function getItemBinNumber(upc) {
-  console.log(getItemByID(upc).binNumber)
+    console.log(getItemByID(upc).binNumber)
 }
 
 function getItemTotalStock(upc) {
-  console.log(getItemByID(upc).totalStock)
+    console.log(getItemByID(upc).totalStock)
 }
 
 function getItemInStock(upc) {
-  console.log(getItemByID(upc).inStock)
+    console.log(getItemByID(upc).inStock)
 }
 
 function getItemOnHand(upc) {
-  console.log(getItemByID(upc).onHand)
+    console.log(getItemByID(upc).onHand)
 }
 
 function isItemConsumable(upc) {
-  console.log(getItemByID(upc).consumable)
+    console.log(getItemByID(upc).consumable)
 }
